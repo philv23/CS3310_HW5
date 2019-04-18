9140e65693db38d14c157de76965e9d78f3e86d2
philv <philv@DESKTOP-1JN3JHC>
2019-04-17 20:45:15 -0400

    ReadFile

--- src/CS3310/wmich/edu/PVarnerKHamelink/Main.java
+++ src/CS3310/wmich/edu/PVarnerKHamelink/Main.java
@@ -1,11 +1,21 @@
 package CS3310.wmich.edu.PVarnerKHamelink;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("YAY!!!!!!!!");
-
+	public static void main(String[] args) throws IOException {
+		RandomAccessFile randFile = new RandomAccessFile("hw5SampleData.dat", "rw");
+		long n = randFile.length();
+		n = n / 4;
+		for(int i = 0; i < n; i++) {
+			int num = randFile.readInt();
+			System.out.println(num);
+		}
+		
+		randFile.close();
 	}
 
 }
